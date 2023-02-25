ff3947b732e5c7d098bb9b6cfccb27513b3f7ab8
lzh <lzh@qq.com>
2023-02-25 11:07:35 +0800

    gitTest1.0 commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author 刘政浩
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
